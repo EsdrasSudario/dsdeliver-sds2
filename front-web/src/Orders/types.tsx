081fe4e4fd9caceacde4689b0d4bff546de7eb36
EsdrasSudario <esdrassudario@gmail.com>
2021-01-10 13:37:10 -0300

    Search order location

---
+++ front-web/src/Orders/types.tsx
@@ -0,0 +1,13 @@
+export type Product = {
+  id: number;
+  name: string;
+  price: number;
+  description: String;
+  imageUri: string;
+}
+
+export type OrderLocationdata = {
+  latitude: number;
+  longitude: number;
+  address: string;
+}
